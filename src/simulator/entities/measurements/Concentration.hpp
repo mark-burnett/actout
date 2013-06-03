d1d872c1b3f6edaf1f65f34ed16023fd8edf56e0
Mark Burnett <mark.m.burnett@gmail.com>
2013-06-03 07:28:40 -0500

    add concentration measurement

---
+++ src/simulator/entities/measurements/Concentration.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "entities/State.hpp"
+#include "entities/measurements/SingleValueBase.hpp"
+#include "entities/common.hpp"
+
+#include <cmath>
+#include <inttypes.h>
+#include <vector>
+
+
+namespace entities {
+namespace measurements {
+
+
+class Concentration : public SingleValueBase {
+public:
+    Concentration(double const& sample_interval, species_t const& species)
+        : SingleValueBase(sample_interval), species_(species) {}
+
+private:
+    double const get_value(State const* state) const {
+        return state->concentrations[species_]->value();
+    }
+
+private:
+    species_t const species_;
+};
+
+
+} // namespace entities
+} // namespace measurements
---
+++ test/simulator/entities/measurements/TestConcentration.cpp
@@ -0,0 +1,42 @@
+#include "entities/State.hpp"
+#include "entities/measurements/Concentration.hpp"
+#include "entities/state/VariableConcentration.hpp"
+
+#include <boost/assign/std/vector.hpp>
+#include <gtest/gtest.h>
+#include <inttypes.h>
+#include <vector>
+
+// Bring operator+= into the namespace
+using namespace boost::assign;
+
+using namespace entities;
+
+
+TEST(ConcentrationMeasurment, Basic) {
+    // Need a state w/ one concentration
+    State s;
+    s.concentrations.push_back(std::move(
+                std::unique_ptr<state::VariableConcentration>(
+                    new state::VariableConcentration(2, 1))));
+
+    measurements::Concentration m(1, 0);
+    for (uint64_t i = 0; i < 11; ++i) {
+        s.time = 0.5 * i;
+        m.perform(&s);
+        s.concentrations[0]->add_monomer();
+    }
+
+    std::vector<double> expected_values;
+    expected_values += 2, 4, 6, 8, 10, 12;
+    ASSERT_EQ(expected_values, m.samples);
+
+    for (uint64_t i = 0; i < 6; ++i) {
+        s.time = 5 + i;
+        m.perform(&s);
+        s.concentrations[0]->remove_monomer();
+    }
+
+    expected_values += 12, 11, 10, 9, 8;
+    ASSERT_EQ(expected_values, m.samples);
+}
